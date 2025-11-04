34ea7eddc5da808a940f1a2025a4ad6857e28939
Yoan Lechevalier <yoan.lechevalier@gmail.com>
2025-11-04 15:58:13 +0100

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react()],
-  base: "Portfolio/",
+  base: "/Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
