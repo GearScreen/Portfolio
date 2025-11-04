8df97787b6e7542bc8be09e1ecf3eac2f070680f
Yoan Lechevalier <yoan.lechevalier@gmail.com>
2025-11-04 15:54:36 +0100

    Update Config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react()],
-  base: "/",
+  base: "Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
