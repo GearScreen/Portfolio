d0f1284f21e83d0defae473c7f152e4a0c44c7a2
Yoan Lechevalier <yoan.lechevalier@gmail.com>
2025-11-04 15:16:15 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
-  base: "/Portfolio/", // /Portfolio/
+  plugins: [react()],
+  base: mode === 'production' ? "/Portfolio/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist'
+  }
 }));
